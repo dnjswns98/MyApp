417a8ffd191fddd0455db19ae3506e775715bc65
dnjswns98 <tngkrt2004@gmail.net>
2022-10-13 17:51:52 +0900

    Login & Register

--- app/src/main/java/com/example/myapp/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/myapp/Activity/RegisterActivity.java
@@ -1,16 +1,106 @@
 package com.example.myapp.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText nickname, email, phoneNum, password, username;
+    Button btn_register;
+
+    private FirebaseAuth auth;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        nickname = findViewById(R.id.et_Nickname);
+        email = findViewById(R.id.et_email);
+        phoneNum = findViewById(R.id.et_callNum);
+        password = findViewById(R.id.et_pwd);
+        username = findViewById(R.id.et_name);
+        btn_register = findViewById(R.id.btn_register);
+
+        auth = FirebaseAuth.getInstance();
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_nickname = nickname.getText().toString();
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                String txt_phoneNum = phoneNum.getText().toString();
+                String txt_username = username.getText().toString();
+
+                if (TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)
+                        || TextUtils.isEmpty(txt_nickname) || TextUtils.isEmpty(txt_phoneNum)){
+                    Toast.makeText(RegisterActivity.this,"빈칸을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 6) {
+                    Toast.makeText(RegisterActivity.this,"비밀번호를 6자리 이상 입력해주세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    register(txt_username, txt_email, txt_password, txt_phoneNum, txt_username);
+                }
+            }
+        });
+    }
+
+    private void register(String nickname, String email, String password, String phoneNum, String username){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                    assert firebaseUser != null;
+                    String userid = firebaseUser.getUid();
+
+                    reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("id", userid);
+                    hashMap.put("nickname", nickname);
+                    hashMap.put("phoneNum", phoneNum);
+                    hashMap.put("username", username);
+                    hashMap.put("imageURL","default");
+                    hashMap.put("status","offline");
+                    hashMap.put("search", nickname.toLowerCase());
+
+                    reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(RegisterActivity.this, "가입 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myapp/Memberinfo.java
@@ -0,0 +1,47 @@
+package com.example.myapp;
+
+public class Memberinfo {
+    private String id;
+    private String nickname;
+    private String username;
+    private String phoneNum;
+    private String imageURL;
+    private String status;
+    private String search;
+
+    public Memberinfo(String id, String nickname, String username, String phoneNum, String imageURL, String status, String search){
+        this.id = id;
+        this.nickname = nickname;
+        this.username = username;
+        this.phoneNum = phoneNum;
+        this.imageURL = imageURL;
+        this.status = status;
+        this.search = search;
+    }
+    public Memberinfo() {
+
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getNickname() { return nickname; }
+    public void setNickname(String nickname) { this.nickname = nickname; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPhoneNum() { return phoneNum; }
+    public void setPhoneNum(String phoneNum) { this.phoneNum = phoneNum; }
+
+    public String getImageURL() { return imageURL; }
+    public void setImageURL(String imageURL) { this.imageURL = imageURL; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public String getSearch() { return search; }
+    public void setSearch(String search) { this.search = search; }
+
+
+}
