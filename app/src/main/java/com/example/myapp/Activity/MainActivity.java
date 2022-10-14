98f5edcb94d14750c16eb5f22065c950a87863d6
dnjswns98 <tngkrt2004@gmail.net>
2022-10-14 18:20:51 +0900

    HorizontalScrollView

--- app/src/main/java/com/example/myapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapp/Activity/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.myapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.media.session.MediaController;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 import com.example.myapp.R;
 
 public class MainActivity extends BasicActivity {
 
+    private VideoView videoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
