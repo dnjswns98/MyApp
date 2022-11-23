bff957497c2cfbd3f352311cfae9ff503418f9c5
dnjswns98 <tngkrt2004@gmail.net>
2022-11-24 03:24:25 +0900

    +회원탈퇴

--- app/src/main/java/com/example/myapp/Activity/CommunityActivity.java
+++ app/src/main/java/com/example/myapp/Activity/CommunityActivity.java
@@ -1,4 +1,17 @@
 package com.example.myapp.Activity;
 
-public class CommunityActivity {
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+
+import com.example.myapp.R;
+
+public class CommunityActivity extends BasicActivity{
+    @Override
+    //어플 생명주기, 사용자가 다른 어플 사용하거나 통화를 하거나 등 다른 활동을 할때 문제 해결
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community);
+    }
 }
