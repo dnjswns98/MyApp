7b5986ab9764304883721d2fbf8d54a798b2b5ba
박시현 <gkrry1117@gmail.com>
2022-11-23 23:56:12 +0900

    report activity(월별)

--- app/src/main/java/com/example/myapp/Activity/ReportActivity.java
+++ app/src/main/java/com/example/myapp/Activity/ReportActivity.java
@@ -1,4 +1,109 @@
 package com.example.myapp.Activity;
 
-public class ReportActivity {
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+import com.example.myapp.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+
+import java.util.ArrayList;
+
+public class ReportActivity extends BasicActivity{
+    //선 그래프
+    private LineChart lineChart;
+    Button btn_home,btn_setting, btn_community, btn_menu;
+    Button btn_detail_report;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        ArrayList<Entry> entry_chart1 = new ArrayList<>(); // 데이터를 담을 Arraylist
+
+
+        lineChart = (LineChart) findViewById(R.id.chart);
+
+        LineData chartData = new LineData(); // 차트에 담길 데이터
+
+        entry_chart1.add(new Entry(1, 1)); //entry_chart1에 좌표 데이터를 담는다.
+        entry_chart1.add(new Entry(2, 2));
+        entry_chart1.add(new Entry(3, 3));
+        entry_chart1.add(new Entry(4, 4));
+        entry_chart1.add(new Entry(5, 2));
+        entry_chart1.add(new Entry(6, 8));
+
+
+
+        LineDataSet lineDataSet1 = new LineDataSet(entry_chart1, "LineGraph1"); // 데이터가 담긴 Arraylist 를 LineDataSet 으로 변환한다.
+
+        lineDataSet1.setColor(Color.BLUE); // 해당 LineDataSet의 색 설정 :: 각 Line 과 관련된 세팅은 여기서 설정한다.
+
+
+        chartData.addDataSet(lineDataSet1); // 해당 LineDataSet 을 적용될 차트에 들어갈 DataSet 에 넣는다.
+
+
+        lineChart.setData(chartData); // 차트에 위의 DataSet을 넣는다.
+
+        lineChart.invalidate(); // 차트 업데이트
+        lineChart.setTouchEnabled(false); // 차트 터치 disable
+
+
+        btn_setting = findViewById(R.id.btn_setting);
+        btn_home = findViewById(R.id.btn_home);
+        btn_community = findViewById(R.id.btn_community);
+        btn_menu = findViewById(R.id.btn_menu);
+        btn_detail_report = findViewById(R.id.btn_detail_report);
+
+        btn_detail_report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ReportActivity.this, DetailReportActivity.class));
+            }
+        });
+
+
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+                startActivity(new Intent(ReportActivity.this, SettingActivity.class));
+            }
+        });
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ReportActivity.this, MainActivity.class));
+            }
+        });
+        btn_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ReportActivity.this, MenuActivity.class));
+            }
+        });
+        btn_community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ReportActivity.this, CommunityActivity.class));
+            }
+        });
+
+
+    }
+
 }
