1d1d307b4aa985a29e89ffddb8090840b3f51ce2
박시현 <gkrry1117@gmail.com>
2022-11-25 02:10:02 +0900

    +캘린더틀,사전틀

--- app/src/main/java/com/example/myapp/Activity/FoodDictionaryActivity.java
+++ app/src/main/java/com/example/myapp/Activity/FoodDictionaryActivity.java
@@ -5,7 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.myapp.R;
+import com.example.myapp.adapter.SimpleTextAdapter;
+
+import java.util.ArrayList;
 
 public class FoodDictionaryActivity extends BasicActivity {
     Button btn_home,btn_setting, btn_community, btn_menu;
@@ -45,5 +51,18 @@ public class FoodDictionaryActivity extends BasicActivity {
                 startActivity(new Intent(FoodDictionaryActivity.this, CommunityActivity.class));
             }
         });
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i=0; i<100; i++) {
+            list.add(String.format("TEXT %d", i)) ;
+        }
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_food) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)) ;
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        SimpleTextAdapter adapter = new SimpleTextAdapter(list) ;
+        recyclerView.setAdapter(adapter) ;
     }
 }
