0e44a93fcea69d75970bae7b96d6c4ebcae83fa5
하은 <lhe.010223@daum.net>
2022-11-26 04:34:56 +0900

    11/25 menu-운동 알람

--- app/src/main/java/com/example/myapp/Activity/ExerAlarmActivity.java
+++ app/src/main/java/com/example/myapp/Activity/ExerAlarmActivity.java
@@ -1,25 +1,45 @@
 package com.example.myapp.Activity;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import java.text.DateFormat;
+import java.util.Calendar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 
+import com.example.myapp.Fragments.AlertReceiver;
+import com.example.myapp.Fragments.NotificationHelper;
+import com.example.myapp.Fragments.TimePickerFragment;
 import com.example.myapp.R;
 
-public class ExerAlarmActivity extends BasicActivity {
-    Button btn_home,btn_setting, btn_community, btn_menu;
+public class ExerAlarmActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
+    Button btn_home,btn_setting, btn_community, btn_menu, button, buttonCancelAlarm ;
+    TextView mTextView;
+    NotificationHelper mNotificationHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_exer_alarm);
+        mNotificationHelper = new NotificationHelper(this);
+        mTextView =  findViewById(R.id.textView);
 
-
+        button = findViewById(R.id.button_timepicker);
         btn_setting = findViewById(R.id.btn_setting);
         btn_home = findViewById(R.id.btn_home);
         btn_community = findViewById(R.id.btn_community);
         btn_menu = findViewById(R.id.btn_menu);
+        buttonCancelAlarm = findViewById(R.id.button_cancel);
+
         btn_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,5 +65,62 @@ public class ExerAlarmActivity extends BasicActivity {
                 startActivity(new Intent(ExerAlarmActivity.this, CommunityActivity.class));
             }
         });
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+
+        });
+
+        buttonCancelAlarm.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick (View v) {
+                cancelAlarm();
+            }
+        });
     }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+
+        updateTimeText(c);
+        startAlarm(c);
+    }
+
+    private void updateTimeText(Calendar c){
+        String timeText = "Alarm set for : ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+
+        mTextView.setText(timeText);
+    }
+
+    private void startAlarm(Calendar c){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        if(c.before((Calendar.getInstance()))){
+            c.add(Calendar.DATE, 1);
+        }
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+        //alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), 1*60*1000 ,  pendingIntent);
+
+    }
+
+    private void cancelAlarm(){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        alarmManager.cancel(pendingIntent);
+        mTextView.setText("취소되었습니다.");
+    }
+
 }
