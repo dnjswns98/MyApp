f52648cec8a826292c132c0bc0fe0e860a370ea4
하은 <lhe.010223@daum.net>
2022-11-27 01:51:00 +0900

    11/27-운동 알람

--- app/src/main/java/com/example/myapp/Fragments/AlertReceiver.java
+++ app/src/main/java/com/example/myapp/Fragments/AlertReceiver.java
@@ -3,7 +3,6 @@ package com.example.myapp.Fragments;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
 import androidx.core.app.NotificationCompat;
 
 public class AlertReceiver extends BroadcastReceiver {
--- app/src/main/java/com/example/myapp/Fragments/NotificationHelper.java
+++ app/src/main/java/com/example/myapp/Fragments/NotificationHelper.java
@@ -6,17 +6,17 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.os.Build;
-import com.example.myapp.R;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 
-public class NotificationHelper extends ContextWrapper {
-    public static final String channel1ID = "channel1ID";
-    public static final String channel1Name = "channel 1 ";
-    public static final String channel2ID = "channel2ID";
-    public static final String channel2Name = "channel 2 ";
+import com.example.myapp.R;
 
-    private NotificationManager mManager;
+public class NotificationHelper extends ContextWrapper {
+    public static final String channeID = "channelID";
+    public static final String channeNm = "channelNm";
+
+    private NotificationManager notiManager;
 
     public NotificationHelper(Context base) {
         super(base);
@@ -25,49 +25,35 @@ public class NotificationHelper extends ContextWrapper {
         }
     }
 
-    public void createChannels(){
-        NotificationChannel channel1 = new NotificationChannel(channel1ID,channel1Name, NotificationManager.IMPORTANCE_DEFAULT);
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void createChannels() {
+        NotificationChannel channel1 = new NotificationChannel(channeID, channeNm, NotificationManager.IMPORTANCE_DEFAULT);
         channel1.enableLights(true);
         channel1.enableVibration(true);
-        channel1.setLightColor(R.color.black);
+        channel1.setLightColor(com.google.android.material.R.color.design_default_color_on_primary);
         channel1.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
 
         getManager().createNotificationChannel(channel1);
-
-        NotificationChannel channel2 = new NotificationChannel(channel2ID,channel2Name, NotificationManager.IMPORTANCE_DEFAULT);
-        channel2.enableLights(true);
-        channel2.enableVibration(true);
-        channel2.setLightColor(R.color.black);
-        channel2.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-
-        getManager().createNotificationChannel(channel2);
-
     }
 
     public NotificationManager getManager() {
-        if (mManager == null){
-            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notiManager == null) {
+            notiManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         }
 
-        return mManager;
+        return notiManager;
     }
 
-//    public NotificationCompat.Builder getChannel1Notification(String title, String message){
-//        return new NotificationCompat.Builder(getApplicationContext(), channel1ID)
-//                .setContentTitle(title)
-//                .setContentText(message)
-//                .setSmallIcon(R.drawable.loginbg2);
-//    }
-//
-//    public NotificationCompat.Builder getChannel2Notification(String title, String message){
-//        return new NotificationCompat.Builder(getApplicationContext(), channel2ID)
-//                .setContentTitle(title)
-//                .setContentText(message)
-//                .setSmallIcon(R.drawable.loginbg2);
-//    }
-
-    public NotificationCompat.Builder getChannelNotification(){
-        return new NotificationCompat.Builder(getApplicationContext(), channel1ID)
+    public NotificationCompat.Builder getChannel1Notification(String title, String message) {
+        return new NotificationCompat.Builder(getApplicationContext(), channeID)
+                .setContentTitle("알람")
+                .setContentText("운동할 시간입니다!")
                 .setSmallIcon(R.drawable.loginbg2);
     }
+
+    public NotificationCompat.Builder getChannelNotification(){
+        return new NotificationCompat.Builder(getApplicationContext(), channeID)
+                .setSmallIcon(R.drawable.loginbg2);
+    }
+
 }
